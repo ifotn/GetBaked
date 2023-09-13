3ed289b5c828d2e5f48dbe304914417802a65529
ifotn <rich@infrontofthenet.com>
2023-09-13 15:50:13 -0400

    l2/2 - start shop controller

--- GetBaked/Controllers/ShopController.cs
+++ GetBaked/Controllers/ShopController.cs
@@ -8,5 +8,13 @@ namespace GetBaked.Controllers
         {
             return View();
         }
+
+        // accept a string parameter to read the "name" value from the url
+        public IActionResult ByCategory(string name)
+        {
+            // store the selected category name in the ViewData object for display on the page
+            ViewData["Category"] = name;
+            return View();
+        }
     }
 }
