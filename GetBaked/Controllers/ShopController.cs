4e41833b101c1474c6552ed40e1182f7b1665c1d
ifotn <rich@infrontofthenet.com>
2023-09-13 14:57:11 -0400

    Add project files.

---
+++ GetBaked/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GetBaked.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
