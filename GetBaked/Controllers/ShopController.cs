6ec1343590fa27b7f808c038620e184d368c4fbc
ifotn <rich@infrontofthenet.com>
2023-09-13 16:41:21 -0400

    l2/3 - shop by category

--- GetBaked/Controllers/ShopController.cs
+++ GetBaked/Controllers/ShopController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GetBaked.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GetBaked.Controllers
 {
@@ -14,7 +15,20 @@ namespace GetBaked.Controllers
         {
             // store the selected category name in the ViewData object for display on the page
             ViewData["Category"] = name;
-            return View();
+
+            // use our Product model to create an in-memory list of products
+            // this will be replaced with a database query in week 4
+            // first define an empty list of Product objects
+            var products = new List<Product>();
+
+            // use a loop to create some products and add each 1 to the list
+            for (int i = 1; i <=10; i++)
+            {
+                products.Add(new Product { ProductId = i, Name = "Product " + i.ToString(), Price = 8 });
+            }
+
+            // send the products list to the view for display
+            return View(products);
         }
     }
 }
