0aceb4e750364617491fb996142f9f346a713117
ifotn <rich@infrontofthenet.com>
2023-11-08 14:50:52 -0500

    l9/1 - rebuild shop pages to be dynamic

--- GetBaked/Controllers/ShopController.cs
+++ GetBaked/Controllers/ShopController.cs
@@ -1,31 +1,46 @@
-﻿using GetBaked.Models;
+﻿using GetBaked.Data;
+using GetBaked.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GetBaked.Controllers
 {
     public class ShopController : Controller
     {
-        public IActionResult Index()
+        // add db connection whenever an instance of the controller is created
+        private readonly ApplicationDbContext _context;
+
+        // constructor method - runs automatically when controller instance created
+        public ShopController(ApplicationDbContext context)
         {
-            return View();
+            _context = context; 
         }
 
-        // accept a string parameter to read the "name" value from the url
-        public IActionResult ByCategory(string name)
+        public IActionResult Index()
         {
-            // store the selected category name in the ViewData object for display on the page
-            ViewData["Category"] = name;
+            // use db connection to fetch then display list of categories
+            var categories = _context.Categories.OrderBy(c => c.Name).ToList();
 
-            // use our Product model to create an in-memory list of products
-            // this will be replaced with a database query in week 4
-            // first define an empty list of Product objects
-            var products = new List<Product>();
+            // pass the categories to the view for display
+            return View(categories);
+        }
 
-            // use a loop to create some products and add each 1 to the list
-            for (int i = 1; i <=10; i++)
-            {
-                products.Add(new Product { ProductId = i, Name = "Product " + i.ToString(), Price = 8 });
+        // accept an int parameter to read the "id" value from the url
+        public IActionResult ByCategory(int id)
+        {
+            // look up the category by the id, store name in the ViewData object for display on the page
+            var category = _context.Categories.Find(id);
+
+            // return to /shop if category not found
+            if (category == null) {
+                return RedirectToAction("Index");
             }
+
+            ViewData["Category"] = category.Name;
+
+            // use our Product model to get the products in the selected category
+            var products = _context.Products.Where(p => p.CategoryId == id)
+                .OrderBy(p => p.Name)
+                .ToList();
 
             // send the products list to the view for display
             return View(products);
